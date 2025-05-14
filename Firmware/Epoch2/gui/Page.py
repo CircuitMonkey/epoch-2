4ceebcb1e8980970041884cdd8238632fbce1556
Mark J Koch <maehem@users.noreply.github.com>
2025-05-14 11:34:06 -0700

    Add GUI and Motors update methods.

--- Firmware/Epoch2/gui/Page.py
+++ Firmware/Epoch2/gui/Page.py
@@ -4,13 +4,19 @@ class Page(displayio.Group):
     def __init__(self):
         super().__init__()
 
-    def destroy():
+    def destroy(self):
         return
 
     def handleTouch( self, touch, drag ):
         print("Page Touches")
 
         return 1
+
+    def updateGUI(self):
+        return
+
+    def updateMotors(self, motors):
+        return
 
     def clearTouch( self ):
         # print("Touch ended")
