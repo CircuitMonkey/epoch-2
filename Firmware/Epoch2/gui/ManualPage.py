8720b6d44800808dfcf17c0f71740c6fc47fd14d
Mark J Koch <maehem@users.noreply.github.com>
2025-05-15 20:56:06 -0700

    Rework page. Add group selectors.

--- Firmware/Epoch2/gui/ManualPage.py
+++ Firmware/Epoch2/gui/ManualPage.py
@@ -3,6 +3,7 @@ from gui.Page import Page
 from gui.Widgets import Slider
 from gui.Widgets import Indicator
 from gui.Widgets import ImageButton
+from gui.Widgets import TextButton
 
 class ManualPage(Page):
     def __init__(self, state, glyphs_img, glyphs_palette, font):
@@ -10,14 +11,13 @@ class ManualPage(Page):
         self.state = state
         self.dragChannel = 0
         self.state.pause = True
+        self.slider_group = 0 # 0:1..4, 1: 5..8, 2:9..12,  3:12..16
 
         self.sliders = [
-            Slider( 0, 64, glyphs_img, glyphs_palette, 28, font ),
-            Slider( 64, 64, glyphs_img, glyphs_palette, 29, font ),
-            Slider( 128, 64, glyphs_img, glyphs_palette, 30, font ),
-            Slider( 196, 64, glyphs_img, glyphs_palette, 31, font ),
-            Slider( 256, 64, glyphs_img, glyphs_palette, 34, font ),
-            Slider( 320, 64, glyphs_img, glyphs_palette, 47, font ),
+            Slider( 64, 64, glyphs_img, glyphs_palette, 1, font ),
+            Slider( 128, 64, glyphs_img, glyphs_palette, 2, font ),
+            Slider( 192, 64, glyphs_img, glyphs_palette, 3, font ),
+            Slider( 256, 64, glyphs_img, glyphs_palette, 4, font ),
         ]
 
         for i, e in enumerate(self.sliders):
@@ -31,13 +31,18 @@ class ManualPage(Page):
         self.append(self.pauseButton)
         self.append(self.returnButton)
 
+        # Left/Right Buttons
+        self.leftButton = TextButton(0,64,1,"<", glyphs_img, glyphs_palette,font)
+        self.rightButton = TextButton(320,64,1,">", glyphs_img, glyphs_palette,font)
+        self.append(self.leftButton)
+        self.append(self.rightButton)
+        self.updateLeftRight()
+
+
     def destroy(self):
-        self.remove(self.sliders[0])
-        self.remove(self.sliders[1])
-        self.remove(self.sliders[2])
-        self.remove(self.sliders[3])
-        self.remove(self.sliders[4])
-        self.remove(self.sliders[5])
+        for i, e in enumerate(self.sliders):
+            e.set_slider_value(state.mode_manual_slider[i])
+            self.remove(e)
         self.remove(self.indicator.group)
         self.remove(self.pauseButton)
         self.remove(self.returnButton)
@@ -51,14 +56,24 @@ class ManualPage(Page):
 
     def updateMotors(self, motors):
         if self.state.pause:
-            for ch in range(0,7):
+            for ch in range(0,15):
                 motors.setMotor(ch, 0)
         else:
-            for ch in range(0, 7, 1):
-                motors.setMotor(ch, self.state.mode_manual_slider[ch]) # 0-99
-                # motors.setMotor(ch + 1, state.mode_manual_slider[ch]) # 0-99
+            for ch in range(0, 3):
+                motors.setMotor(self.slider_group*4+ch, self.state.mode_manual_slider[ch]) # 0-99
 
         return
+
+    def updateLeftRight(self):
+                if self.slider_group < 1:
+                    self.leftButton.hidden = True
+                else:
+                    self.leftButton.hidden = False
+
+                if self.slider_group > 3:
+                    self.rightButton.hidden = True
+                else:
+                    self.rightButton.hidden = False
 
     def togglePause(self):
         self.state.pause = not self.state.pause
@@ -77,6 +92,7 @@ class ManualPage(Page):
         if drag > 0 and self.dragChannel == 0:
             print("Nothing to drag")
             return 1 # nothing to drag
+        txO = touch[0]
         tx = touch[0] - self.x
         ty = touch[1]
         print(f"Handle ManualMode Touches => X: {tx}, Y: {ty}")
@@ -85,40 +101,64 @@ class ManualPage(Page):
             if tx > self.returnButton.x and tx < self.returnButton.x+64:
                 self.state.pause = True
                 return 2
-        elif ty > 416 and ty < 480:
+        if ty > 416 and ty < 480:
             if tx > self.pauseButton.x and tx < self.pauseButton.x+(4*64):
                 self.togglePause()
                 return 1 # Handled and now its a drag.
+        if ty > self.leftButton.y and ty < self.leftButton.y+64:
+            if txO > self.leftButton.x and txO < self.leftButton.x+64:
+                print("Touch Left")
+                self.slider_group -= 1
+                if self.slider_group < 0:
+                    self.slider_group = 0
+                if self.slider_group > 3:
+                    self.slider_group = 3
+                for i in range(4):
+                    self.sliders[i].set_label(self.slider_group*4 + i + 1)
+
+                self.updateLeftRight()
+
+                return 1 # Handled and now its a drag.
+
+        if ty > self.rightButton.y and ty < self.rightButton.y+64:
+            if txO > self.rightButton.x and txO < self.rightButton.x+64:
+                print("Touch Right")
+                self.slider_group += 1
+                if self.slider_group < 0:
+                    self.slider_group = 0
+                if self.slider_group > 3:
+                    self.slider_group = 3
+                for i in range(4):
+                    self.sliders[i].set_label(self.slider_group*4 + i + 1)
+
+                self.updateLeftRight()
+
+                return 1 # Handled and now its a drag.
+
+
         if (self.dragChannel == 0 or self.dragChannel == 1) and self.sliders[0].handleTouch(touch, drag) > 0:
             self.dragChannel = 1
+            stateSlider = self.slider_group * 4 + 0
             self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
+            # print( f"drag slider {self.dragChannel}" )
             return 1
         if (self.dragChannel == 0 or self.dragChannel == 2) and self.sliders[1].handleTouch(touch, drag) > 0:
             self.dragChannel = 2
+            stateSlider = self.slider_group * 4 + 0
             self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
+            # print( f"drag slider {self.dragChannel}" )
             return 1
         if (self.dragChannel == 0 or self.dragChannel == 3) and self.sliders[2].handleTouch(touch, drag) > 0:
             self.dragChannel = 3
+            stateSlider = self.slider_group * 4 + 0
             self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
+            # print( f"drag slider {self.dragChannel}" )
             return 1
         if (self.dragChannel == 0 or self.dragChannel == 4) and self.sliders[3].handleTouch(touch, drag) > 0:
             self.dragChannel = 4
+            stateSlider = self.slider_group * 4 + 0
             self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
-            return 1
-        if (self.dragChannel == 0 or self.dragChannel == 5) and self.sliders[4].handleTouch(touch, drag) > 0:
-            self.dragChannel = 5
-            # TODO: there are actually 10 sliders ch 1-4 and ch5-8 and then delay and speed.
-            self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
-            return 1
-        if (self.dragChannel == 0 or self.dragChannel == 6) and self.sliders[5].handleTouch(touch, drag) > 0:
-            self.dragChannel = 6
-            self.state.mode_manual_slider[self.dragChannel-1] = self.sliders[self.dragChannel-1].value
-            print( f"drag slider {self.dragChannel}" )
+            # print( f"drag slider {self.dragChannel}" )
             return 1
 
         self.dragChannel = 0 # clear drag
